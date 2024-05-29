3d608b2e3b2f8540b1b5daf66d12451bb46eaf60
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-05-29 18:32:39 +0500

    check if animations work on prod

--- src/app/components/Stack.tsx
+++ src/app/components/Stack.tsx
@@ -1,6 +1,11 @@
 "use client";
-import gsap from "gsap";
+import { gsap } from "gsap";
 import { useLayoutEffect, useRef } from "react";
+import { MdCircle } from "react-icons/md";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Fragment } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Stack() {
   const component = useRef(null);
@@ -38,6 +43,9 @@ export default function Stack() {
     return () => ctx.revert();
   }, []);
 
+  const tech_name = "React";
+  const tech_color = "#00D1B2";
+
   return (
     <>
       <div className="relative h-screen p-8">
@@ -45,6 +53,30 @@ export default function Stack() {
         <p className="text-sm font-light md:text-2xl">
           Что я использую в работе
         </p>
+        <section className="wrapper overflow-hidden">
+          <div
+            className="tech-row mb-8 flex items-center justify-center gap-4 text-slate-700"
+            aria-label={tech_name || ""}
+          >
+            {Array.from({ length: 15 }, (_, index) => (
+              <Fragment key={index}>
+                <span
+                  className={
+                    "tech-item text-8xl font-extrabold uppercase tracking-tighter"
+                  }
+                  style={{
+                    color: index === 7 && tech_color ? tech_color : "inherit",
+                  }}
+                >
+                  {tech_name}
+                </span>
+                <span className="text-3xl">
+                  <MdCircle />
+                </span>
+              </Fragment>
+            ))}
+          </div>
+        </section>
       </div>
     </>
   );
