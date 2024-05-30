1f1105dda6651c84a039f2074b2eb4f2c3587512
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-05-30 21:25:23 +0500

    еуьздфеу кщгештп

--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -1,7 +1,12 @@
 export default function Pets() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Pets</h1>
+    <div className="z-10 flex min-h-screen flex-col">
+      <div className="container mx-auto p-4 md:p-20">
+        <h1 className="text-4xl font-bold">Проекты</h1>
+        <p className="mt-4 text-lg">
+          Сайты, над которыми я работаю в данный момент.
+        </p>
+      </div>
     </div>
   );
 }
--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -1,7 +1,47 @@
 export default function Stack() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold">Stack</h1>
+    <div className="z-10 flex min-h-screen flex-col">
+      <div className="container mx-auto p-4 md:p-20">
+        <h1 className="text-4xl font-bold">Технологии</h1>
+        <p className="mt-4 text-lg">
+          Языки, фреймворки, библиотеки и инструменты, которые я использую в
+          проектах.
+        </p>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+          <div className="col-span-1 flex flex-col">
+            <p className="text-lg">
+              <span className="font-bold">TypeScript</span> - язык
+              программирования, который позволяет мне писать более простой и
+              читаемый код.
+            </p>
+            <p className="mt-2 text-lg">
+              <span className="font-bold">Next.js</span> - фреймворк для
+              создания сайтов, который позволяет мне легко создавать приложения.
+            </p>
+          </div>
+          <div className="col-span-1 flex flex-col">
+            <p className="text-lg">
+              <span className="font-bold">Tailwind CSS</span> - CSS-библиотека,
+              которая позволяет мне создавать стили для моих приложений.
+            </p>
+            <p className="mt-2 text-lg">
+              <span className="font-bold">React</span> - библиотека для создания
+              интерфейсов, которая позволяет мне легко создавать
+              пользовательские интерфейсы.
+            </p>
+          </div>
+          <div className="col-span-1 flex flex-col">
+            <p className="text-lg">
+              <span className="font-bold">Vercel</span> - сервис для создания
+              сайтов, который позволяет мне легко создавать приложения.
+            </p>
+            <p className="mt-2 text-lg">
+              <span className="font-bold">GitHub</span> - сервис для хранения и
+              совместной работы над проектами.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
