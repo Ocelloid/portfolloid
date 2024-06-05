4dd185cc10d45b961e6b0d5ed52eb157211904e5
Nikita Grebnev <n.a.grebnev@gmail.com>
2024-06-05 10:53:38 +0500

    admin page fix

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -19,7 +19,6 @@ export default function AdminPage() {
       getInquiries()
         .then((inq: Inquiry[]) => {
           setInquiries(inq);
-          console.log(inq);
         })
         .catch((e) => {
           console.log(e);
@@ -29,31 +28,33 @@ export default function AdminPage() {
 
   return (
     <div className="flex min-h-screen flex-col">
-      <div className="container mx-auto p-4 pb-10 md:p-20">
-        <div className="flex flex-col gap-4 p-4">
-          <h2 className="text-4xl font-bold">Входящие заявки</h2>
-          {inquiries.map((inq) => (
-            <div
-              className="flex flex-col gap-2 rounded-lg bg-slate-600/75 p-4"
-              key={inq.id}
-            >
-              <p className="text-2xl">{inq.username}</p>
-              <div className="-mt-2 flex flex-row items-center gap-2 text-xs">
-                <p>
-                  Email:&nbsp;<a href={`mailto:${inq.email}`}>{inq.email}</a>
-                </p>
-                <p>Компания: {inq.organization}</p>
+      {!!user?.id && user?.id === "user_2hO3y89tdym40T8WxVzfGn2Q06h" && (
+        <div className="container mx-auto p-4 pb-10 md:p-20">
+          <div className="flex flex-col gap-4 p-4">
+            <h2 className="text-4xl font-bold">Входящие заявки</h2>
+            {inquiries.map((inq) => (
+              <div
+                className="flex flex-col gap-2 rounded-lg bg-slate-600/75 p-4"
+                key={inq.id}
+              >
+                <p className="text-2xl">{inq.username}</p>
+                <div className="-mt-2 flex flex-row items-center gap-2 text-xs">
+                  <p>
+                    Email:&nbsp;<a href={`mailto:${inq.email}`}>{inq.email}</a>
+                  </p>
+                  <p>Компания: {inq.organization}</p>
+                </div>
+                {!!inq.services && (
+                  <p className="text-justify">Услуги: {inq.services}</p>
+                )}
+                {!!inq.message && (
+                  <p className="text-justify">Сообщение: {inq.message}</p>
+                )}
               </div>
-              {!!inq.services && (
-                <p className="text-justify">Услуги: {inq.services}</p>
-              )}
-              {!!inq.message && (
-                <p className="text-justify">Сообщение: {inq.message}</p>
-              )}
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
